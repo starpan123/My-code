4a04ea4a846690c78aa1e571ab199c63baa87676
starpan123 <m13110032289@163.com>
2017-01-16 17:31:17 +0800

    Drupal写了一个product版本管理模块

---
+++ Drupal/sites/default/modules/my_product/controllers/ProductEditController.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/1/11
+ * Time: 19:16
+ */
+class ProductEditController {
+   static public function getProductEditForm(){
+
+   }
+}
